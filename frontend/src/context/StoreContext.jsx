982662b5f3410ce5110d79375b8f0a8987a6cbc2
Amit rawat <157109376+amit-rawat09@users.noreply.github.com>
2025-03-11 12:34:54 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cardItem, setCardItem] = useState({})
-    const url = 'http://localhost:400'
+    const url = 'https://food-application-fvi8.onrender.com'
     const [token, setToken] = useState("")
 
     const [food_list, setFood_list] = useState([])
