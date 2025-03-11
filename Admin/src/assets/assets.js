7f0100b7f041e2c3b2a05747696e82baf80abfbc
Amit rawat <157109376+amit-rawat09@users.noreply.github.com>
2025-03-11 12:36:59 +0530

    Update assets.js

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -3,6 +3,8 @@ import admin from './profile_img.jpeg'
 import upload from './upload.png'
 import parcel from './parcel.png'
 
+export const url="https://food-application-fvi8.onrender.com"
+
 export const assets={
     logo,
     admin,
